7b5f9e5f3853a7e1274db08f8dd43cd1045c57c5
Subramaniyam Raizada <subraizada3@gmail.com>
2019-01-11 02:06:39 -0500

    updated keymap

--- keyboards/viterbi/keymaps/subraizada3/keymap.c
+++ keyboards/viterbi/keymaps/subraizada3/keymap.c
@@ -11,61 +11,58 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Note: don't have F12 conflict with Delete key on second layer                                                            macros
- * ,---F1------F2------F3------F4------F5------F6------------------.      ,---F7------F8------F9-----F10-----F11-------------F12----------.
- * |   ~   |   1   |   2   |   3   |   4   |   5   |XXXXXXX|XXXXXXX|      |   6   |   7   |   8   |   9   |   0   | delete|   A   |XXXXXXX| A: Media +
+/*
+ * ,-----------F1------F2------F3------F4------F5------------------.      ,---F6------F7------F8----- F9-----F10-----F11-----F12----------.
+ * |   ~   |   1   |   2   |   3   |   4   |   5   |XXXXXXX|XXXXXXX|      |   6   |   7   |   8   |   9   |   0   |   -   |   +   |XXXXXXX|
  * |---------------------------------------------------------------|      |---------------------------------------------------------------|
- * |  tab  |   q   |   w   |   f   |   p   |   g   |XXXXXXX|XXXXXXX|      |   k   |   n   |   u   |   y   |   ;   |   L0  |   B   |XXXXXXX| B: Media -
+ * |  tab  |   q   |   w   |   e   |   r   |   t   |XXXXXXX|XXXXXXX|      |   y   |   u   |   i   |   o   |   p   |   {   |   }   |XXXXXXX|
  * |---------------------------------------------------------------|      |---------------------------------------------------------------|
- * |  esc  |   a   |   r   |   s   |   t   |   d   |XXXXXXX|XXXXXXX|      |   h   |   l   |   e   |   i   |   o   |  L1h  |   C   |XXXXXXX| C: Media play/pause
+ * |  esc  |   a   |   s   |   d   |   f   |   g   |XXXXXXX|XXXXXXX|      |   h   |   j   |   k   |   l   |   ;   |   "   |  L1h  |XXXXXXX|
  * |---------------------------------------------------------------|      |---------------------------------------------------------------|
- * | shift |   z   |   x   |   c   |   v   |   b   |XXXXXXX|XXXXXXX|      |   j   |   m   |   ,   |   .   |   /   | shift |   D   |XXXXXXX| D: Volume +
+ * | shift |   z   |   x   |   c   |   v   |   b   |XXXXXXX|XXXXXXX|      |   n   |   m   |   ,   |   .   |   /   | shift |  del  |XXXXXXX|
  * |---------------------------------------------------------------|      |---------------------------------------------------------------|
- * |  ctrl |  L1h  |  win  |  alt  | space | enter |XXXXXXX|XXXXXXX|      |  bksp | space |  L1t  |  alt  |  win  |  ctrl |   E   |XXXXXXX| E: Volume -
+ * |  ctrl |  L1h  |  win  |  alt  |  bksp | space |XXXXXXX|XXXXXXX|      | space | enter |  alt  |  win  |  ctrl |  ctrl |   \   |XXXXXXX|
  * `---------------------------------------------------------------'      `---------------------------------------------------------------'
  */
   [0] = LAYOUT(
 // ,-------|-------|-------|-------|-------|-------|-------|-------.      ,-------|-------|-------|-------|-------|-------|-------|-------.
-     KC_GRV,  KC_1 ,  KC_2 ,  KC_3 ,  KC_4 ,  KC_5 ,XXXXXXX,XXXXXXX,         KC_6 ,  KC_7 ,  KC_8 ,  KC_9 ,  KC_0 , KC_DEL,KC_MNXT,XXXXXXX,
+     KC_GRV,  KC_1 ,  KC_2 ,  KC_3 ,  KC_4 ,  KC_5 ,XXXXXXX,XXXXXXX,         KC_6 ,  KC_7 ,  KC_8 ,  KC_9 ,  KC_0 ,KC_MINS, KC_EQL,XXXXXXX,
 // |-------|-------|-------|-------|-------|-------|-------|-------|      |-------|-------|-------|-------|-------|-------|-------|-------|
-//     KC_TAB,  KC_Q ,  KC_W ,  KC_F ,  KC_P ,  KC_G ,XXXXXXX,XXXXXXX,         KC_K ,  KC_N ,  KC_U ,  KC_Y ,KC_SCLN, TO(0) ,KC_WHOM,XXXXXXX,
-     KC_TAB,  KC_Q ,  KC_W ,  KC_E ,  KC_R ,  KC_T ,XXXXXXX,XXXXXXX,         KC_Y ,  KC_U ,  KC_I ,  KC_O ,  KC_P , TO(0) ,KC_MPRV,XXXXXXX,
+     KC_TAB,  KC_Q ,  KC_W ,  KC_E ,  KC_R ,  KC_T ,XXXXXXX,XXXXXXX,         KC_Y ,  KC_U ,  KC_I ,  KC_O ,  KC_P ,KC_LBRC,KC_RBRC,XXXXXXX,
 // |-------|-------|-------|-------|-------|-------|-------|-------|      |-------|-------|-------|-------|-------|-------|-------|-------|
-//     KC_ESC,  KC_A ,  KC_R ,  KC_S ,  KC_T ,  KC_D ,XXXXXXX,XXXXXXX,         KC_H ,  KC_L ,  KC_E ,  KC_I ,  KC_O , MO(1) ,LCA(KC_T),XXXXXXX,
-     KC_ESC,  KC_A ,  KC_S ,  KC_D ,  KC_F ,  KC_G ,XXXXXXX,XXXXXXX,         KC_H ,  KC_J ,  KC_K ,  KC_L ,KC_SCLN, MO(1) ,KC_MPLY,XXXXXXX,
+     KC_ESC,  KC_A ,  KC_S ,  KC_D ,  KC_F ,  KC_G ,XXXXXXX,XXXXXXX,         KC_H ,  KC_J ,  KC_K ,  KC_L ,KC_SCLN,KC_QUOT, MO(1) ,XXXXXXX,
 // |-------|-------|-------|-------|-------|-------|-------|-------|      |-------|-------|-------|-------|-------|-------|-------|-------|
-//    KC_LSFT,  KC_Z ,  KC_X ,  KC_C ,  KC_V ,  KC_B ,XXXXXXX,XXXXXXX,         KC_J ,  KC_M ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,KC_VOLU,XXXXXXX,
-    KC_LSFT,  KC_Z ,  KC_X ,  KC_C ,  KC_V ,  KC_B ,XXXXXXX,XXXXXXX,         KC_N ,  KC_M ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT,KC_VOLU,XXXXXXX,
+    KC_LSFT,  KC_Z ,  KC_X ,  KC_C ,  KC_V ,  KC_B ,XXXXXXX,XXXXXXX,         KC_N ,  KC_M ,KC_COMM, KC_DOT,KC_SLSH,KC_RSFT, KC_DEL,XXXXXXX,
 // |-------|-------|-------|-------|-------|-------|-------|-------|      |-------|-------|-------|-------|-------|-------|-------|-------|
-    KC_LCTL, MO(1) ,KC_LWIN,KC_LALT,KC_BSPC, KC_SPC,XXXXXXX,XXXXXXX,        KC_SPC, KC_ENT, TG(1) ,KC_RALT,KC_RWIN,KC_RCTL,KC_VOLD,XXXXXXX
+    KC_LCTL, MO(1) ,KC_LWIN,KC_LALT,KC_BSPC, KC_SPC,XXXXXXX,XXXXXXX,        KC_SPC, KC_ENT,KC_RALT,KC_RWIN,KC_RCTL,KC_RCTL,KC_BSLS,XXXXXXX
 // `-------|-------|-------|-------|-------|-------|-------|-------'      `-------|-------|-------|-------|-------|-------|-------|-------'
   ),
 
 
-/* Note: don't have F12 conflict with Delete key on second layer                                                            macros
+/*
  * ,---------------------------------------------------------------.      ,---------------------------------------------------------------.
- * |   F1  |   F2  |   F3  |   F4  |   F5  |   F6  |XXXXXXX|XXXXXXX|      |   F7  |   F8  |   F9  |  F10  |  F11  | delete|  F12  |XXXXXXX| A: F12 (don't confict with Delete key)
+ * |prtscrn|   F1  |   F2  |   F3  |   F4  |   F5  |XXXXXXX|XXXXXXX|      |   F6  |   F7  |   F8  |   F9  |  F10  |  F11  |  F12  |XXXXXXX|
  * |---------------------------------------------------------------|      |---------------------------------------------------------------|
- * |  tab  |  home |   ^   |  end  |  pgup |   (   |XXXXXXX|XXXXXXX|      |   -   |   {   |   [   |   ]   |   |   |   L0  |   B   |XXXXXXX| B: Print screen
+ * |  tab  |  home |   ^   |  end  |  pgup |  volu |XXXXXXX|XXXXXXX|      |   +   |   -   |   _   |   =   |       |       |       |XXXXXXX|
  * |---------------------------------------------------------------|      |---------------------------------------------------------------|
- * |  esc  |   <   |   v   |   >   |  pgdn |   )   |XXXXXXX|XXXXXXX|      |   <   |   v   |   ^   |   >   |   &   |  L1h  |   C   |XXXXXXX| C:
+ * |  esc  |   <   |   v   |   >   |  pgdn |  mute |XXXXXXX|XXXXXXX|      |   <   |   v   |   ^   |   >   |       |       |  L1h  |XXXXXXX|
  * |---------------------------------------------------------------|      |---------------------------------------------------------------|
- * | shift |   $   |   %   |   ^   |   =   |   +   |XXXXXXX|XXXXXXX|      |   _   |   }   |   '   |   "   |   \   | shift |   D   |XXXXXXX| D:
+ * | shift |  prv  |  pau  |  nxt  |       |  vold |XXXXXXX|XXXXXXX|      |   L0  |perm-L1| insert|       |       | shift |  del  |XXXXXXX|
  * |---------------------------------------------------------------|      |---------------------------------------------------------------|
- * |  ctrl |  L1h  |  win  |  alt  | space | enter |XXXXXXX|XXXXXXX|      |  bksp | space |  L1t  |  alt  |  win  |  ctrl |   E   |XXXXXXX| E:
+ * |  ctrl |  L1h  |  win  |  alt  |  bksp | space |XXXXXXX|XXXXXXX|      | space | enter |  alt  |  win  |  ctrl |  ctrl |       |XXXXXXX|
  * `---------------------------------------------------------------'      `---------------------------------------------------------------'
  */
   [1] = LAYOUT(
 // ,-------|-------|-------|-------|-------|-------|-------|-------.      ,-----------|-----------|-----------|-----------|-----------|-----------|-----------|-------.
-     KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,XXXXXXX,XXXXXXX,          KC_F7   ,   KC_F8   ,   KC_F9   ,   KC_F10  ,   KC_F11  ,   KC_DEL  ,  KC_F12   ,XXXXXXX,
+    KC_PSCR, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,XXXXXXX,XXXXXXX,          KC_F6   ,   KC_F7   ,   KC_F8   ,    KC_F9  ,   KC_F10  ,   KC_F11  ,  KC_F12   ,XXXXXXX,
 // |-------|-------|-------|-------|-------|-------|-------|-------|      |-----------|-----------|-----------|-----------|-----------|-----------|-----------|-------|
-     KC_TAB,KC_HOME, KC_UP , KC_END,KC_PGUP,S(KC_9),XXXXXXX,XXXXXXX,         KC_MINS  , S(KC_LBRC),  KC_LBRC  ,  KC_RBRC  , S(KC_BSLS),   TO(0)   ,  KC_PSCR  ,XXXXXXX,
+     KC_TAB,KC_HOME, KC_UP , KC_END,KC_PGUP,KC_VOLU,XXXXXXX,XXXXXXX,        S(KC_EQL) ,  KC_MINS  , S(KC_MINS),   KC_EQL  ,  XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,XXXXXXX,
 // |-------|-------|-------|-------|-------|-------|-------|-------|      |-----------|-----------|-----------|-----------|-----------|-----------|-----------|-------|
-     TG(1) ,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,S(KC_0),XXXXXXX,XXXXXXX,         KC_LEFT  ,  KC_DOWN  ,   KC_UP   ,  KC_RGHT  ,  S(KC_7)  ,  XXXXXXX  ,  XXXXXXX  ,XXXXXXX,
+     TO(0) ,KC_LEFT,KC_DOWN,KC_RGHT,KC_PGDN,KC_MUTE,XXXXXXX,XXXXXXX,         KC_LEFT  ,  KC_DOWN  ,   KC_UP   ,  KC_RGHT  ,  XXXXXXX  ,  XXXXXXX  ,  XXXXXXX  ,XXXXXXX,
 // |-------|-------|-------|-------|-------|-------|-------|-------|      |-----------|-----------|-----------|-----------|-----------|-----------|-----------|-------|
-    KC_LSFT,S(KC_4),S(KC_5),S(KC_6),KC_EQL,S(KC_EQL),XXXXXXX,XXXXXXX,       S(KC_MINS), S(KC_RBRC),  KC_QUOT  ,S(KC_QUOTE),  KC_BSLS  ,  KC_RSFT  ,  XXXXXXX  ,XXXXXXX,
+    KC_LSFT,KC_MPRV,KC_MPLY,KC_MNXT,XXXXXXX,KC_VOLD,XXXXXXX,XXXXXXX,          TO(0)   ,   TG(1)   ,   KC_INS  ,  XXXXXXX  ,  XXXXXXX  ,  KC_RSFT  ,   KC_DEL  ,XXXXXXX,
 // |-------|-------|-------|-------|-------|-------|-------|-------|      |-----------|-----------|-----------|-----------|-----------|-----------|-----------|-------|
-    KC_LCTL,XXXXXXX,KC_LWIN,KC_LALT,KC_BSPC, KC_SPC,XXXXXXX,XXXXXXX,          KC_SPC  ,   KC_ENT  ,   TG(1)   ,  KC_RALT  ,  KC_RWIN  ,  KC_RCTL  ,  XXXXXXX  ,XXXXXXX
+    KC_LCTL,XXXXXXX,KC_LWIN,KC_LALT,KC_BSPC, KC_SPC,XXXXXXX,XXXXXXX,          KC_SPC  ,   KC_ENT  ,  KC_RALT  ,  KC_RWIN  ,  KC_RCTL  ,  KC_RCTL  ,  XXXXXXX  ,XXXXXXX
 // `-------|-------|-------|-------|-------|-------|-------|-------'      `-----------|-----------|-----------|-----------|-----------|-----------|-----------|-------'
   ),
 
